7909c57a43f5dde6bd2929b21adf7cf1ef598bf6
Messaoudi Rym <rym.sara.messaoudi@gmail.com>
2025-01-17 01:50:16 +0100

    config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    NEO4J_URI = "bolt://localhost:7687"  # Adresse de Neo4j
-    NEO4J_USER = "neo4j"                # Nom d'utilisateur par défaut
-    NEO4J_PASSWORD = "rym1902200319"        # Changez par votre mot de passe
+    NEO4J_URI = "bolt://localhost:7687"  
+    NEO4J_USER = "neo4j"               
+    NEO4J_PASSWORD = "rym123"       
