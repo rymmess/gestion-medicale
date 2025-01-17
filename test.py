b60e7661bb761c1c7eca450bcfa5aed652f346a6
Messaoudi Rym <rym.sara.messaoudi@gmail.com>
2025-01-17 01:49:39 +0100

    test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from neo4j import GraphDatabase
 
 try:
-    driver = GraphDatabase.driver("bolt://localhost:7687", auth=("neo4j", "rym1902200319"))
+    driver = GraphDatabase.driver("bolt://localhost:7687", auth=("neo4j", "rym123"))
     with driver.session() as session:
         result = session.run("RETURN 'Connection successful' AS message")
         for record in result:
